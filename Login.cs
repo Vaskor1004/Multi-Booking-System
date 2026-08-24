41b99662b934c44551112ad3e5ba68e2945898f8
Vaskor1004 <vaskorsaha5@gmail.com>
2026-08-24 19:56:28 +0600

    login page updated

--- Login.cs
+++ Login.cs
@@ -1,118 +1,4 @@
-﻿/*using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Configuration;
-using System.Windows.Forms;
-using System.Xml.Linq;
-
-namespace Multi_Booking_System
-{
-    public partial class Login : Form
-    {
-        public Login()
-        {
-            InitializeComponent();
-        }
-
-        private void guna2Button1_Click(object sender, EventArgs e)
-        {
-            string ConnectionString = "Data Source=LAPTOP-ETFSEMF8;Initial Catalog=mydb;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
-
-            string query = @"SELECT COUNT(*)
-                     FROM users
-                     WHERE username = @name
-                     AND password = @password";
-
-            using (SqlConnection con = new SqlConnection(ConnectionString))
-            {
-                SqlCommand cmd = new SqlCommand(query, con);
-
-                cmd.Parameters.AddWithValue("@name", txtboxUsernml.Text);
-                cmd.Parameters.AddWithValue("@password", txtboxPassl.Text);
-
-                con.Open();
-               //this is nothing commit
-
-               int result = (int)cmd.ExecuteScalar();
-
-                if (result > 0)
-                {
-                    MessageBox.Show("Login Successful!");
-                }
-                else
-                {
-                    MessageBox.Show("Incorrect Name or Password!");
-                }
-            }
-
-       }
-            string ConnectionString =
-               "Data Source=LAPTOP-ETFSEMF8;Initial Catalog=mydb;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
-
-            string query = @"SELECT userType
-                     FROM users
-                     WHERE username = @name
-                     AND password = @password";
-
-            using (SqlConnection con = new SqlConnection(ConnectionString))
-            {
-                SqlCommand cmd = new SqlCommand(query, con);
-
-                cmd.Parameters.AddWithValue("@name", txtboxUsernml.Text);
-                cmd.Parameters.AddWithValue("@password", txtboxPassl.Text);
-
-                con.Open();
-
-                object result = cmd.ExecuteScalar();
-
-                if (result != null)
-                {
-                    string userType = result.ToString();
-
-                    if (userType == "Customer")
-                    {
-                        MessageBox.Show("Customer Login Successful!");
-
-                        // CustomerForm customerForm = new CustomerForm();
-                        // customerForm.Show();
-
-                        this.Hide();
-                    }
-                    else if (userType == "Shopkeeper")
-                    {
-                        MessageBox.Show("Shopkeeper Login Successful!");
-
-                        // ShopkeeperForm shopkeeperForm = new ShopkeeperForm();
-                        // shopkeeperForm.Show();
-
-                        this.Hide();
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Invalid Name or Password!");
-                }
-            }
-
-       private void guna2Button1_Click_1(object sender, EventArgs e)
-       {
-           Form1 form1 = new Form1();
-           form1.Show();
-           this.Hide();
-       }
-           
-           
-        }
-    }
-
-
-}*/
+﻿
 using System;
 using System.Data.SqlClient;
 using System.Windows.Forms;
@@ -121,6 +7,7 @@ namespace Multi_Booking_System
 {
     public partial class Login : Form
     {
+        public static int userId;
         public Login()
         {
             InitializeComponent();
@@ -131,7 +18,7 @@ namespace Multi_Booking_System
             string ConnectionString =
                 "Data Source=LAPTOP-ETFSEMF8;Initial Catalog=mydb;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
 
-            string query = @"SELECT userType
+            string query = @"SELECT id, userType
                              FROM users
                              WHERE username = @name
                              AND password = @password";
@@ -144,12 +31,18 @@ namespace Multi_Booking_System
                 cmd.Parameters.AddWithValue("@password", txtboxPassl.Text);
 
                 con.Open();
+                string userType = null;
+                var reader = cmd.ExecuteReader();
+                
 
-                object result = cmd.ExecuteScalar();
-
-                if (result != null)
+                while (reader.Read())
                 {
-                    string userType = result.ToString();
+                    userId = reader.GetInt32(0);
+                    userType = reader.GetString(1);
+                }
+
+                if (userType != null)
+                {
 
                     if (userType == "Customer")
                     {
